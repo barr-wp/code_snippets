7f3974d8fa722431ce2e73eaced8382b0ef61642
wbarr <barr.wp@gmail.com>
2022-07-30 08:43:57 -0600

    .

--- ArcGIS/point_cloud_qc.py
+++ ArcGIS/point_cloud_qc.py
@@ -1,12 +1,13 @@
 import arcpy.management as mgmt
 from os import path
 
-def pointCloudStats(out_folder, las_dataset, sampling_val, count_limit):
+def pointCloudStats(out_folder, las_dataset, sampling_val, out_name):
 
     # Create statistics rasters
-    las_file_name = path.basename(las_dataset).split('.')[0]
+    # las_file_name = path.basename(las_dataset).split('.')[0]
     print('')
-    out_ras = path.join(out_folder, f'{las_file_name}_PULSE_COUNT.tif')
+    out_ras = path.join(out_folder, f'{out_name}_PULSE_COUNT.tif')
+
     print(out_ras)
     mgmt.LasPointStatsAsRaster(
         las_dataset,
@@ -18,7 +19,7 @@ def pointCloudStats(out_folder, las_dataset, sampling_val, count_limit):
 
     # Create las statistics
     print('exporting statistics')
-    out_stats = path.join(out_folder, f'{las_file_name}_stats.csv')
+    out_stats = path.join(out_folder, f'{out_name}_stats.csv')
     mgmt.LasDatasetStatistics(
         las_dataset,
         calculation_type='SKIP_EXISTING_STATS',
